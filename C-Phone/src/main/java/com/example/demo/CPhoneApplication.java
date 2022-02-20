5ed2721397f9d2d31c8eb48933ebddd18b8c35d4
ryosei <ryo214_007@yahoo.co.jp>
2022-02-21 03:41:14 +0900

    環境構築

---
+++ C-Phone/src/main/java/com/example/demo/CPhoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CPhoneApplication.class, args);
+	}
+
+}
